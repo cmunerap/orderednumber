7e6833b42dc302d6ff901dbb795ed19d72e30d77
cristian.munera <cmunerap@psl.com.co>
2018-07-15 03:24:08 -0500

    Fixes an issue with inputs that are not a number

--- orderednumber.js
+++ orderednumber.js
@@ -45,6 +45,7 @@ fs.readFile(inputFile, encoding, (err, inputContent) => {
 /**
  * Reads a line in a safe way. The purpose is to avoid a possible automatic cast of the input as number
  * @param {string} line - The incomming line to be readed
+ * @return {string} - Line readed as string
  */
 function safeLineRead(line) {
     return ''+line;
@@ -91,7 +92,8 @@ function saveResults(outputContent) {
  */
 function calculateCorrespondingOrderedNumber(number) {
     if (+number < 10) return number; // Number lest than 10 is actually an ordered number. Happy path!
-    
+    if (isNaN(number)) return '';
+
     var listOfDigits;               // The digits of the provided number
     var previousDigit = 0;          // Used to check the previoust digit (as number) against the current
     var currentDigit;               // Stores the current digit (as number) to be validated in the sequence
